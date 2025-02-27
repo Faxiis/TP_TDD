7d5cf02a2de4a3c92596b683099574e54d6cc56a
Faxiis <antho.lavoute@gmail.com>
2025-02-27 12:00:48 +0100

    Isbn validator fixed

--- TP_TDD/Validators/IsbnValidator.cs
+++ TP_TDD/Validators/IsbnValidator.cs
@@ -4,6 +4,11 @@ public class IsbnValidator
 {
     public bool IsValid(string isbn)
     {
+        if (isbn.Any(c => !char.IsDigit(c)))
+        {
+            return false;
+        }
+        
         return isbn.Length == 10 || isbn.Length == 13;
     }
 }
--- TP_TDD/obj/Debug/net8.0/TP_TDD.AssemblyInfo.cs
+++ TP_TDD/obj/Debug/net8.0/TP_TDD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_TDD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfeae0d85d8ff786ce843c17709ba3619648ad3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27c158919fb7040f8f4ae0743278199b4bd1e9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_TDD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_TDD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
