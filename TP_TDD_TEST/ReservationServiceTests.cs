a1dce48f8bc2fd9ecd1c625e10155bbbedf37143
Faxiis <antho.lavoute@gmail.com>
2025-02-27 16:09:49 +0100

    AddReservation_ShouldNotAddReservationToLibrary_MemberHas3Reservations added

--- TP_TDD_TEST/ReservationServiceTests.cs
+++ TP_TDD_TEST/ReservationServiceTests.cs
@@ -82,4 +82,28 @@ public class ReservationServiceTests
 
         Assert.IsTrue(reservation.IsReturned);
     }
+    
+    // test qu'un adhérent ne peut pas avoir plus de 3 réservations en cours
+    [Test]
+    public void AddReservation_ShouldNotAddReservationToLibrary_MemberHas3Reservations()
+    {
+        var author = new Author() { Id = 1, LastName = "Test Author", FirstName = "Test Author" };
+        var publisher = new Publisher() { Siret = "1234567890", Name = "Test Publisher" };
+        var book = new Book() { Isbn = "2253009687", Title = "Test Book", Author = author, Publisher = publisher, Format = "Poche", IsAvailable = true };
+        var member = new Member() { Code = 1, LastName = "Test Member", FirstName = "Test Member", BirthDate = new DateTime(2000, 1, 1), Civility = "M" };
+        var reservation1 = new Reservation() { Id = 1, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
+        var reservation2 = new Reservation() { Id = 2, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
+        var reservation3 = new Reservation() { Id = 3, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
+        var reservation = new Reservation() { Id = 4, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
+
+        _mockDatabaseService.Setup(service => service.GetReservationById(1)).Returns(reservation1);
+        _mockDatabaseService.Setup(service => service.GetReservationById(2)).Returns(reservation2);
+        _mockDatabaseService.Setup(service => service.GetReservationById(3)).Returns(reservation3);
+        
+        var reservationService = new ReservationService(_mockDatabaseService.Object);
+        reservationService.AddReservation(reservation);
+
+        _mockDatabaseService.Verify(service => service.AddReservation(It.IsAny<Reservation>()), Times.Never);;
+        Assert.IsNull(reservationService.GetReservationById(4));
+    }
 }
--- TP_TDD_TEST/obj/Debug/net8.0/TP_TDD_TEST.AssemblyInfo.cs
+++ TP_TDD_TEST/obj/Debug/net8.0/TP_TDD_TEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_TDD_TEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94513f3df05237aabf666ee14d179190d2963a83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4097dcedc6e5250347cecc64b1a0a57930189c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_TDD_TEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_TDD_TEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
