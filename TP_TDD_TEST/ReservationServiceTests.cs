2205dcd6f03ec26bfb65c6b552159c9fb4a44c3c
Faxiis <antho.lavoute@gmail.com>
2025-02-27 16:24:45 +0100

    GetReservation_ShouldReturnReservations added

--- TP_TDD_TEST/ReservationServiceTests.cs
+++ TP_TDD_TEST/ReservationServiceTests.cs
@@ -103,4 +103,24 @@ public class ReservationServiceTests
         _mockDatabaseService.Verify(service => service.AddReservation(It.IsAny<Reservation>()), Times.Never);
         Assert.IsNull(reservationService.GetReservationById(4));
     }
+    
+    // test Il est possible d’obtenir la liste des réservations ouvertes
+    [Test]
+    public void GetReservation_ShouldReturnReservations()
+    {
+        var author = new Author() { Id = 1, LastName = "Test Author", FirstName = "Test Author" };
+        var publisher = new Publisher() { Siret = "1234567890", Name = "Test Publisher" };
+        var book = new Book() { Isbn = "2253009687", Title = "Test Book", Author = author, Publisher = publisher, Format = "Poche", IsAvailable = true };
+        var member = new Member() { Code = 1, LastName = "Test Member", FirstName = "Test Member", BirthDate = new DateTime(2000, 1, 1), Civility = "M" };
+        var reservation1 = new Reservation() { Id = 1, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
+        var reservation2 = new Reservation() { Id = 2, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
+        var reservation3 = new Reservation() { Id = 3, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
+
+        _mockDatabaseService.Setup(service => service.GetReservations()).Returns(new List<Reservation> { reservation1, reservation2, reservation3 });
+
+        var reservationService = new ReservationService(_mockDatabaseService.Object);
+        var reservations = reservationService.GetReservations();
+
+        Assert.That(reservations, Is.EqualTo(new List<Reservation> { reservation1, reservation2, reservation3 }));
+    }
 }
