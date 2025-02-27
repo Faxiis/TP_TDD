cf28ca1d340e9edb80193667c4a2b523d1240a90
Faxiis <antho.lavoute@gmail.com>
2025-02-27 15:55:29 +0100

    Test added reservation > 4 month

--- TP_TDD_TEST/ReservationServiceTests.cs
+++ TP_TDD_TEST/ReservationServiceTests.cs
@@ -21,7 +21,7 @@ public class ReservationServiceTests
         var publisher = new Publisher() { Siret = "1234567890", Name = "Test Publisher" };
         var book = new Book() { Isbn = "2253009687", Title = "Test Book", Author = author, Publisher = publisher, Format = "Poche", IsAvailable = true };
         var member = new Member() { Code = 1, LastName = "Test Member", FirstName = "Test Member", BirthDate = new DateTime(2000, 1, 1), Civility = "M" };
-        var expectedReservation = new Reservation() { Id = 1, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24) };
+        var expectedReservation = new Reservation() { Id = 1, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
 
         _mockDatabaseService.Setup(service => service.GetReservationById(1)).Returns(expectedReservation);
 
@@ -38,7 +38,7 @@ public class ReservationServiceTests
         var publisher = new Publisher() { Siret = "1234567890", Name = "Test Publisher" };
         var book = new Book() { Isbn = "2253009687", Title = "Test Book", Author = author, Publisher = publisher, Format = "Poche", IsAvailable = true };
         var member = new Member() { Code = 1, LastName = "Test Member", FirstName = "Test Member", BirthDate = new DateTime(2000, 1, 1), Civility = "M" };
-        var reservation = new Reservation() { Id = 1, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24) };
+        var reservation = new Reservation() { Id = 1, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 3, 24) };
 
         _mockDatabaseService.Setup(service => service.GetReservationById(1)).Returns(reservation);
         
@@ -48,5 +48,23 @@ public class ReservationServiceTests
         _mockDatabaseService.Verify(service => service.AddReservation(It.IsAny<Reservation>()), Times.Once);;
         Assert.IsNotNull(reservationService.GetReservationById(1));
         Assert.That(reservationService.GetReservationById(1), Is.EqualTo(reservation));
+    }
+    
+    [Test]
+    public void AddReservation_ShouldNotAddReservationToLibrary_TimeIsSuperiorToForMonth()
+    {
+        var author = new Author() { Id = 1, LastName = "Test Author", FirstName = "Test Author" };
+        var publisher = new Publisher() { Siret = "1234567890", Name = "Test Publisher" };
+        var book = new Book() { Isbn = "2253009687", Title = "Test Book", Author = author, Publisher = publisher, Format = "Poche", IsAvailable = true };
+        var member = new Member() { Code = 1, LastName = "Test Member", FirstName = "Test Member", BirthDate = new DateTime(2000, 1, 1), Civility = "M" };
+        var reservation = new Reservation() { Id = 1, Book = book, Member = member, ReservationDate = new DateTime(2025, 2, 24), ReturnDate = new DateTime(2025, 8, 24) };
+
+        _mockDatabaseService.Setup(service => service.GetReservationById(1)).Returns(reservation);
+        
+        var reservationService = new ReservationService(_mockDatabaseService.Object);
+        reservationService.AddReservation(reservation);
+
+        _mockDatabaseService.Verify(service => service.AddReservation(It.IsAny<Reservation>()), Times.Never);;
+        Assert.IsNull(reservationService.GetReservationById(1));
     }
 }
--- TP_TDD_TEST/obj/Debug/net8.0/TP_TDD_TEST.AssemblyInfo.cs
+++ TP_TDD_TEST/obj/Debug/net8.0/TP_TDD_TEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_TDD_TEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42cdb730c626790be47ef53731dcdc432f357238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89958ba41f54a214e09227f847745ff1733450fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_TDD_TEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_TDD_TEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
