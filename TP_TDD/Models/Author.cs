8feb0a0c9875db0618bbde4983c0702fc39768fe
Faxiis <antho.lavoute@gmail.com>
2025-02-27 11:25:57 +0100

    Publisher and Author entity added

---
+++ TP_TDD/Models/Author.cs
@@ -0,0 +1,8 @@
+namespace TP_TDD.Models;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+}
---
+++ TP_TDD/Models/Publisher.cs
@@ -0,0 +1,7 @@
+namespace TP_TDD.Models;
+
+public class Publisher
+{
+    public required string Siret { get; set; }
+    public string Name { get; set; }
+}
